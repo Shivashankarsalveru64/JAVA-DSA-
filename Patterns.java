5a22b84a31155d3d9370c55744adf24b6dfa09d4
Shivashankar Salveru <YOUR_GITHUB_EMAIL>
2026-08-22 14:12:04 +0530

    initial commit

---
+++ Patterns.java
@@ -0,0 +1,79 @@
+public class Patterns {
+    public static void main(String[] args) {
+        int n=4;int m=5;
+        //Solid Rectangle
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //Hollow Rectangle
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1||j==1||i==n||j==m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        //half Pyramid
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        //Inverted Half Pyramid
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        //inverted Half Pyramid 180 Degrees
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        //numbers
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println(" ");
+        }
+        int num =1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+        //0,1 triangle
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        }
+
+
+    }
+}
